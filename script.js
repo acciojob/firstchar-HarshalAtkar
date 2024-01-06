577716caf96f55ffa07009637803cd0891492794
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-01-06 22:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function firstChar(text) {
-  // your code herefunction firstChar(inputString) {
+  // your code here
     for (let i = 0; i < text.length; i++) {
         const char = text.charAt(i);
         if (char !== ' ') {
             return char;
         }
     }
-    return ''; // If all characters are spaces
+    return ''; 
 
 }
 
